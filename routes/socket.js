a2a82de716e27e5bd818e7d15d020d6ab89a7dbf
Kazuma Nishigaki <nishigaki7315@outlook.com>
2018-11-20 13:26:12 +0900

    chat

--- routes/socket.js
+++ routes/socket.js
@@ -1,13 +1,12 @@
 var express = require('express');
+
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/con', function(req, res, next) {
   res.render('index', { title: 'Express' });
+    
 });
 
-router.get('/test', function(req, res, next) {
-  res.render('test', { title: 'Express' });
-});
 
 module.exports = router;
