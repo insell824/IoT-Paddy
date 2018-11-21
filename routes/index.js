8866044e3093803cbde9b59a8a58768574f67f44
Kazuma Nishigaki <nishigaki7315@outlook.com>
2018-11-21 18:11:55 +0900

    add view of console

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-
+var os = require('os');
+var hostname = os.hostname();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -8,6 +9,10 @@ router.get('/', function(req, res, next) {
 
 router.get('/test', function(req, res, next) {
   res.render('test', { title: 'Express' });
+});
+
+router.get('/console', function(req, res, next) {
+  res.render('root/console', { title: 'Express', contents:hostname });
 });
 
 module.exports = router;
--- routes/socket.js
+++ routes/socket.js
@@ -1,11 +1,14 @@
 var express = require('express');
 
 var router = express.Router();
+var os = require('os');
+var hostname = os.hostname();
 
 /* GET home page. */
 router.get('/con', function(req, res, next) {
+  console.log(hostname);
   res.render('index', { title: 'Express' });
-    
+ 
 });
 
 
