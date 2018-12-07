93462da3f434024f4d36327c0c10c15e0bf35615
Kazuma Nishigaki <nishigaki7315@outlook.com>
2018-12-08 04:50:48 +0900

    new type of sigfox message history

--- awssns.js
+++ awssns.js
@@ -1,15 +1,25 @@
 var AWS = require('aws-sdk');
 AWS.config.loadFromPath('./aws.json');
-AWS.config.update({region: 'us-west-2'});
-var params = {
-  PhoneNumber: '+819079608067',
-  Message: "2田んぼの様子が変わりました．\n専用端末かWebから確認できます．\nhttp://iot-paddy.herokuapp.com"
-};
-var publishTextPromise = new AWS.SNS({apiVersion: '2010-03-31'}).publish(params).promise();
-publishTextPromise.then(function(data) {
-    console.log("Message "+params.Message+" send sent to the topic "+params.TopicArn);
+AWS.config.update({region: 'us-east-1'});
+
+const sendMessage = function (msg=null){
+  var params = {
+    PhoneNumber: '+81-80-1981-9098',
+    Message: "2田んぼの様子が変わりました．\n専用端末かWebから確認できます．\nhttp://iot-paddy.herokuapp.com"
+  };
+  if(msg != null){
+    params.Message = msg;
+  }
+  var publishTextPromise = new AWS.SNS({apiVersion: '2010-03-31'}).publish(params).promise();
+
+  publishTextPromise.then(function(data) {
+    console.log("Message "+params.Message);
     console.log("MessageID is "+data.MessageId);
   }).catch(function(err) {
     console.error(err, err.stack);
-  }
-);
+  });
+}
+
+module.exports = {
+  sendMessage 
+}
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,21 @@
 var express = require('express');
 var router = express.Router();
 var appenv = require('./lib/common/app-env');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+var myaws = require("../awssns.js");
 express().use(bodyParser.urlencoded({ extended: true }))
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  
   res.render('index', { title: 'Express' });
 });
 
 router.get('/test', function(req, res, next) {
-  res.render('test', { title: 'Express', hostUrl:(req.headers['x-forwarded-proto'] + '://' + req.headers.host) });
+  res.render('test', { title: 'Express', hostUrl:(process.env.URL || "http://localhost:3000") });
+});
+
+router.get('/push', function(req, res, next) {
+  res.render('root/push', { title: 'Express', hostUrl:(process.env.URL || "http://localhost:3000") });
 });
 
 router.get('/console', function(req, res, next) {
@@ -28,14 +34,13 @@ router.get('/client', function(req,res, nexr){
   res.render('root/sample');
 });
 
+// Sigfoからメッセージが届く
 router.post('/client', function(req,res, next){
   var io = require('socket.io-client');
   var socket = io(process.env.URL || "http://localhost:3000");
   socket.on('connect', function () {
-    socket.emit("sendMessageToServer", {value:JSON.stringify(req.body)});
+    socket.emit("newSigfoxObj", req.body);
   });
-  //console.log("HERE:");
-  //console.log(JSON.stringify(req.body));
   res.send('OK');
 });
 
