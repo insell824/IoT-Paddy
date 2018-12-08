52b1d4d521be0b14d80dd52358406ee6e70c4b77
Kazuma Nishigaki <nishigaki7315@outlook.com>
2018-12-08 10:20:35 +0900

    useast

--- awssns.js
+++ awssns.js
@@ -1,6 +1,6 @@
 var AWS = require('aws-sdk');
 const fs = require('fs');
-const region_ = "us-east-1";
+const region_ = "us-east-2";
 try {
   fs.statSync('./aws.json');
   AWS.config.loadFromPath('./aws.json');
